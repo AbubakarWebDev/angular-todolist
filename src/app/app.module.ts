6b41010df4336d18c98a9869ab5fe833f253956e
Muhammad Abubakar <muhammad.abubakar@reolustech.net>
2022-01-27 20:13:48 +0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FontColorAppComponent } from './font-color-app/font-color-app.component';
-import { FormHandlingComponent } from './form-handling/form-handling.component';
 import { TodolistComponent } from './todolist/todolist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FontColorAppComponent,
-    FormHandlingComponent,
     TodolistComponent,
   ],
   imports: [
